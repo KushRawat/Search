6b03695c4617a4c810f3cc3b65f4b9f0a979b470
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-06-15 18:06:24 +0530

    removing previous searched result

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const form = document.querySelector('#searchForm')
+const resultsDiv = document.querySelector('#results')
+
 form.addEventListener('submit', async function(e) {
     e.preventDefault() //PREVENTING THE FORM FROM REQUESTING ACTION
+    resultsDiv.innerHTML = '' // TO CLEAR PREVIOUS SEARCHED RESULT
+    
     // console.dir(form) GIVES ALL ELEMENTS AND DETAILS OF FORM 
     // console.dir(form.elements.query.value) WHAT USER SEARCHES FOR
     const searchTerm = form.elements.query.value
@@ -10,19 +14,19 @@ form.addEventListener('submit', async function(e) {
     // const config = {params: {q: searchTerm, isFunny: 'Kush'}} // to add multiple things as query string instead of adding it to the url
     const res = await axios.get(`http://api.tvmaze.com/search/shows`, config) // config is an axios object
     // const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`)
-    searchData(res.data) 
+    showData(res.data) 
     // console.log(res)
     // console.log(res.data) // SEARCHED SHOW DATA
     // console.log(res.data[0].show.image.medium) // SHOWS SHOW IMAGE    
 })
 
- const searchData = shows => {
+ const showData = shows => {
     for(let result of shows) {
         // console.log(result) //testing
         if(result.show.image) {
             const img = document.createElement('IMG')
             img.src = result.show.image.medium
-            document.body.append(img)
+            resultsDiv.append(img)
         }
     }
  } 
