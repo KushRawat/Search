45a68390d585881f6702294d7d268c3b887f35d5
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-06-28 02:30:25 +0530

    adding quote feature

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const resultsDiv = document.querySelector("#results");
 form.addEventListener("input", async function (e) {
   e.preventDefault(); //PREVENTING THE FORM FROM REQUESTING ACTION
   resultsDiv.innerHTML = ""; // TO CLEAR PREVIOUS SEARCHED RESULT
+  document.getElementById("quoteTop").style.display = "none";
+  document.getElementById("quoteBottom").style.display = "none";
 
   // console.dir(form) GIVES ALL ELEMENTS AND DETAILS OF FORM
   // console.dir(form.elements.query.value) WHAT USER SEARCHES FOR
@@ -15,18 +17,18 @@ form.addEventListener("input", async function (e) {
   const res = await axios.get(`http://api.tvmaze.com/search/shows`, config); // config is an axios object
   // const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`)
   showData(res.data);
-  console.log(res.data);
-  console.log(res.data[0].show.genres);
-  console.log(res.data[0].show.name);
-  console.log(res.data[0].show.network.name);
-  console.log(res.data[0].show.officialSite);
-  console.log(res.data[0].show.rating.average);
-  console.log(res.data[0].show.premiered);
-  console.log(res.data[0].show.schedule);
-  console.log(res.data[0].show.schedule.time);
-  console.log(res.data[0].show.schedule.days);
-  console.log(res.data[0].show.status);
-  console.log(res.data[0].show.summary);
+  // console.log(res.data);
+  // console.log(res.data[0].show.genres);
+  // console.log(res.data[0].show.name);
+  // console.log(res.data[0].show.network.name);
+  // console.log(res.data[0].show.officialSite);
+  // console.log(res.data[0].show.rating.average);
+  // console.log(res.data[0].show.premiered);
+  // console.log(res.data[0].show.schedule);
+  // console.log(res.data[0].show.schedule.time);
+  // console.log(res.data[0].show.schedule.days);
+  // console.log(res.data[0].show.status);
+  // console.log(res.data[0].show.summary);
   // console.log(res.data) // SEARCHED SHOW DATA
   // console.log(res.data[0].show.image.medium) // SHOWS SHOW IMAGE
   // form.elements.query.value = ''
@@ -35,63 +37,70 @@ form.addEventListener("input", async function (e) {
 const showData = (shows) => {
   for (let result of shows) {
     // console.log(result) //testing
+    document.getElementById("quoteTop").style.display = "block";
+    document.getElementById("quoteBottom").style.display = "block";
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    resultsDiv.append(card);
+
     const br = document.createElement("br");
-    resultsDiv.append(br);
+    card.append(br);
+
     if (result.show.image) {
       const img = document.createElement("IMG");
       img.src = result.show.image.medium;
-      resultsDiv.append(img);
+      card.append(img);
     }
     if (!result.show.image) {
       const img = document.createElement("IMG");
       img.src = "https://tinyurl.com/tv-missing";
-      resultsDiv.append(img);
+      card.append(img);
     }
     if (result.show.name) {
       const name = document.createElement("h2");
       name.setAttribute("class", "title");
       name.innerHTML = result.show.name;
-      resultsDiv.append(name);
+      card.append(name);
     }
     if (result.show.genres) {
       const genres = document.createElement("div");
       genres.innerHTML = result.show.genres;
-      resultsDiv.append(genres);
+      card.append(genres);
     }
     if (result.show.network.name) {
       const networkName = document.createElement("div");
       networkName.innerHTML = result.show.network.name;
-      resultsDiv.append(networkName);
+      card.append(networkName);
     }
     if (result.show.officialSite) {
       const site = document.createElement("a");
       site.setAttribute("href", result.show.officialSite);
       site.setAttribute("class", "link");
       site.innerHTML = result.show.officialSite;
-      resultsDiv.append(site);
+      card.append(site);
     }
     if (result.show.rating.average) {
       const ratings = document.createElement("div");
       ratings.innerHTML = result.show.rating.average;
-      resultsDiv.append(ratings);
+      card.append(ratings);
     }
     if (result.show.premiered) {
       const premiered = document.createElement("div");
       premiered.innerHTML = result.show.premiered;
-      resultsDiv.append(premiered);
+      card.append(premiered);
     }
     if (result.show.status) {
       const status = document.createElement("div");
       status.innerHTML = result.show.status;
-      resultsDiv.append(status);
+      card.append(status);
     }
     if (result.show.summary) {
       const summary = document.createElement("div");
       summary.setAttribute("class", "summary");
       summary.innerHTML = result.show.summary;
-      resultsDiv.append(summary);
+      card.append(summary);
     }
-    const hr = document.createElement("hr");
-    resultsDiv.append(hr);
+    const b = document.createElement("br");
+    resultsDiv.append(b);
   }
 };
